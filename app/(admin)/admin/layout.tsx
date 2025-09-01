746b4fcdd9b360bc9a76ae30478d3f3655762788
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-09-01 23:40:08 +0530

    Admin Page Layout

---
+++ app/(admin)/admin/layout.tsx
@@ -0,0 +1,29 @@
+import { getAdmin } from '@/actions/admin';
+import Header from '@/components/Header';
+import { notFound } from 'next/navigation';
+import React from 'react';
+import SideBar from './_components/SideBar';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AdminLayout = async ({ children }: Props) => {
+  const admin = await getAdmin();
+
+  if (!admin.authorized) {
+    return notFound();
+  }
+
+  return (
+    <div className='h-full'>
+      <Header isAdminPage={true} />
+      <div className='flex h-full w-56 flex-col top-20 fixed inset-y-0 z-50'>
+        <SideBar />
+      </div>
+      <main className='md:pl-56 pt-[80px] h-full '>{children}</main>
+    </div>
+  );
+};
+
+export default AdminLayout;
