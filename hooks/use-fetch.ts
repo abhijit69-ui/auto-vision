89cf83e40e49f8bcfaef37103ae865d745939afc
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-09-05 12:08:20 +0530

    custom hook to fetch data

--- hooks/use-fetch.ts
+++ hooks/use-fetch.ts
@@ -1,7 +1,39 @@
 import { useState } from 'react';
+import { toast } from 'sonner';
 
-const useFetch = () => {
-  const [data, setData] = useState(undefined);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
-};
+// generic callback type
+type AsyncCallback<TArgs extends unknown[], TResult> = (
+  ...args: TArgs
+) => Promise<TResult>;
+
+function useFetch<TArgs extends unknown[], TResult>(
+  cb: AsyncCallback<TArgs, TResult>
+) {
+  const [data, setData] = useState<TResult | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fn = async (...args: TArgs) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);
+      setData(response);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err);
+        toast.error(err.message);
+      } else {
+        setError(new Error('Unknown error'));
+        toast.error('Unknown error');
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn, setData };
+}
+
+export default useFetch;
